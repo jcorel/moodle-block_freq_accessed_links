888d524b097798efd91dba5984771069f65a2a9d
Shadman Ahmed <72008371+shadman-ahmed-bs23@users.noreply.github.com>
2023-08-31 19:28:10 +0600

    Add plugin release version

--- version.php
+++ version.php
@@ -25,5 +25,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_freq_accessed_links';
+$plugin->release = "1.0.0";
 $plugin->version = 2023080403;
 $plugin->requires = 2020061500;
